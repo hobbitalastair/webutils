6dc8eabdc22e9d397cb87f7712d7e9a4d340e6fe
Alastair Hughes <hobbitalastair@gmail.com>
2017-10-04 20:10:49 +1300

    webtoon.sh, join.sh: removed old scripts

--- join.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# This uses imagemagick's convert to join the images in the given directory
-# together. It assumes that the images are numerically ordered.
-
-pushd "$1" > /dev/null
-convert $(echo * | tr ' ' '\n' | rev | cut -d/ -f1 | rev | sort -n) -append \
-    "joined.jpg"
